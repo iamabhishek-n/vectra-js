7fadbfcda8df5b612691e2abde89b1838379aab0
Abhishek N <astroabhi.abhi@gmail.com>
2026-08-08 09:22:57 +0530

    chore: configure Jest and Babel to handle ES module dependencies (uuid)

--- jest.config.js
+++ jest.config.js
@@ -1,4 +1,8 @@
 module.exports = {
   testEnvironment: 'node',
   testMatch: ['**/test/**/*.test.js'],
+  transform: {
+    '^.+\\.m?js$': 'babel-jest',
+  },
+  transformIgnorePatterns: ['node_modules/(?!(uuid)/)'],
 };
